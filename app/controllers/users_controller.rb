5f2b4616f2dda254615acc4b59a17aae43f5c0f0
Phuong Dang <phuongdang@admins-MacBook-Air-2.local>
2018-01-25 18:47:01 +0700

    add friend and view friends portfolio functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,38 @@ class UsersController < ApplicationController
     @user = current_user
     @user_stocks = current_user.stocks
   end
+
+  def my_friends
+    @friendships = current_user.friends
+  end
+
+  def search
+    if params[:search_param].blank?
+      flash.now[:danger] = "You need to provide a valid entry!"
+    else
+      @users = User.search(params[:search_param])
+      @users = current_user.except_current_user(@users)
+      flash.now[:danger] = "User not found!" if @users.blank?
+    end
+    respond_to do |format|
+      format.js { render partial: 'friends/result' }
+    end
+  end
+
+  def add_friend
+    @friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: @friend.id)
+
+    if current_user.save
+      flash[:success] = "You are friend with #{@friend.full_name} now!"
+    else
+      flash[:danger] = "Something went wrong with your request!"
+    end
+    redirect_to my_friends_path
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @user_stocks = @user.stocks
+  end
 end
